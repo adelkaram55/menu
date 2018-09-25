c7a56410003fbe7a57416acddf5e9104bd839180
adelkaram55 <adelkaram55@gmail.com>
2018-09-26 01:22:02 +0200

    v 0.1

---
+++ js/adel.js
@@ -0,0 +1,11 @@
+var closeButton = document.getElementById('closeButton');
+var openButton = document.getElementById('openButton');
+var nav = document.getElementById('dynamicNav');
+
+function widthChangeout(){
+    nav.style.width = 0;
+}
+function widthChangein(){
+    nav.style.width = "30%";
+ 
+}
